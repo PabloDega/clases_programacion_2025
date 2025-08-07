312ace74c4642e2d1863fd5d961064a899c3f132
Pablo Dega <pepeito@pepito.com>
2025-08-07 14:03:46 -0300

    TP23

--- TP23/script/script1.js
+++ TP23/script/script1.js
@@ -24,3 +24,13 @@ document.querySelector("main h2").textContent = "Nuevo título desde JS";
 document.querySelector("#contenidoPrincipal").textContent += " - Modificado desde JS";
 
 document.querySelector("main").innerHTML += `<p class="parrafo">Parrafo 5</p>`;
+
+let h2 = document.querySelector("h2");
+console.log(h2);
+h2.textContent = "Nuevo título desde JS";
+
+document.querySelector("#textoInput").value = "Texto agregado desde JS";
+
+document.querySelector("#divEstilo").style.backgroundColor = "red";
+
+
