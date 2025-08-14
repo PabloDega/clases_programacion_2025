b05d498bfde46d84b82983aa9621acc7d3ebc5c9
Pablo Dega <pepeito@pepito.com>
2025-08-14 16:13:15 -0300

    TP23

--- TP23/script/script3.js
+++ TP23/script/script3.js
@@ -32,6 +32,30 @@ function validarForm() {
     errores.push("El DNI debe tener 8 digitos");
   }
 
+  const letrasAprobadas = "abcdefghijklmnñopqrstuvwxyzáéíóúäëïöü' ".split("");
+
+  const arrayNombre = nombre.value.split("");
+
+  arrayNombre.forEach((letra) => {
+    //console.log(letra.toLowerCase());
+    let query = letrasAprobadas.indexOf(letra.toLowerCase());
+    if(query === -1){
+      resultado = false;
+      errores.push("El campo nombre solo acepta letras");
+    }
+  });
+  
+  const arrayApellido = apellido.value.split("");
+
+  arrayApellido.forEach((letra) => {
+    //console.log(letra.toLowerCase());
+    let query = letrasAprobadas.indexOf(letra.toLowerCase());
+    if(query === -1){
+      resultado = false;
+      errores.push("El campo apellido solo acepta letras");
+    }
+  });
+
   //console.log(errores);
   mostrarErrores(resultado, errores);
 
@@ -42,8 +66,11 @@ function mostrarErrores(resultado, errores) {
   if (resultado) {
     // mostrar mensaje de exito
     document.querySelector("#respuestas > div").innerHTML = `El formulario fue validado con exito`;
+    document.querySelector("body").style.backgroundColor = "darkcyan";
   } else {
     // mostrar mensaje de error y detalle de errores
     document.querySelector("#respuestas > div").innerHTML = `No se pudo validar el formulario`;
+    document.querySelector("#respuestas > div").innerHTML += `<br>-${errores.join("<br>-")}`;
+    document.querySelector("body").style.backgroundColor = "darkred";
   }
 }
