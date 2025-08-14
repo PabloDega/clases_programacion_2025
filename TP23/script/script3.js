e1b135270f61f6248db0c5ba727d84bad3153bb2
Pablo Dega <pepeito@pepito.com>
2025-08-14 15:02:52 -0300

    TP23

--- TP23/script/script3.js
+++ TP23/script/script3.js
@@ -1,18 +1,49 @@
 document.querySelector("form").addEventListener("submit", (e) => {
-    e.preventDefault();
-    validarFormulario();
+  e.preventDefault();
+  const resultado = validarForm();
+  if (resultado) {
+    console.log("Formulario validado con exito");
+  } else {
+    console.log("No se pudo validar la informacion");
+  }
 });
 
-function validarFormulario(){
-    let resultado = true;
-    let errores = [];
-    const nombre = document.querySelector("#nombre");
-    const apellido = document.querySelector("#apellido");
-    const dni = document.querySelector("#dni");
+function validarForm() {
+  //console.log("Validacion");
+  let resultado = true;
+  let errores = [];
 
-    if(nombre.value === "" || apellido.value === "" || dni.value === ""){
-        console.log("Debe completar todos los campos");
-        resultado = false;
-        errores.push("Debe completar todos los campos");
-    }
+  const nombre = document.querySelector("#nombre");
+  const apellido = document.querySelector("#apellido");
+  const dni = document.querySelector("#dni");
+
+  if (nombre.value === "" || apellido.value === "" || dni.value === "") {
+    resultado = false;
+    errores.push("Debe completar todos los campos del formulario");
+  }
+
+  if (isNaN(dni.value)) {
+    resultado = false;
+    errores.push("El campo DNI solo acepta numeros");
+  }
+
+  if (dni.value.length !== 8) {
+    resultado = false;
+    errores.push("El DNI debe tener 8 digitos");
+  }
+
+  //console.log(errores);
+  mostrarErrores(resultado, errores);
+
+  return resultado;
+}
+
+function mostrarErrores(resultado, errores) {
+  if (resultado) {
+    // mostrar mensaje de exito
+    document.querySelector("#respuestas > div").innerHTML = `El formulario fue validado con exito`;
+  } else {
+    // mostrar mensaje de error y detalle de errores
+    document.querySelector("#respuestas > div").innerHTML = `No se pudo validar el formulario`;
+  }
 }
