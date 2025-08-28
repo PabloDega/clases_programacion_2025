27181ae1ecbb9a5730df661382e2a21f8d5f7e4a
Pablo Dega <pepeito@pepito.com>
2025-08-28 15:52:05 -0300

    TP24

--- TP24/script/script2.js
+++ TP24/script/script2.js
@@ -2,10 +2,10 @@ document.querySelector("#saludar").addEventListener("click", () => {
     const nombre = document.querySelector("#nombre");
     const mensaje = document.querySelector("#saludo #mensaje");
     //console.log(nombre.value);
-    if(nombre.value === ""){
+    if (nombre.value === "") {
         //console.log("Input vacio");
         mensaje.textContent = "Error! escriba su nombre en el input";
-    }else{
+    } else {
         //console.log("input con info: " + nombre.value);
         mensaje.textContent = "Hola " + nombre.value;
     }
@@ -18,9 +18,9 @@ document.querySelector("#calcular").addEventListener("click", () => {
     const leche = 100;
     const manteca = 30;
     const huevo = 1;
-    if(cantidad.value === ""){
+    if (cantidad.value === "") {
         mensaje.textContent = "Error! escriba una cantidad en el input";
-    }else{
+    } else {
         mensaje.innerHTML = `<span class="lista">
             <span>Cantidad de ingredientes</span>
             <span>${papas * cantidad.value} grms de papas</span>
@@ -30,3 +30,36 @@ document.querySelector("#calcular").addEventListener("click", () => {
         </span>`;
     }
 })
+
+let asistentes = [];
+
+document.querySelector("#agregar").addEventListener("click", () => {
+    //console.log("Boton agregar clickeado")
+    const asistente = document.querySelector("#asistente");
+    const mensaje = document.querySelector("#listaAsistencia #mensaje");
+    if (asistente.value === "") {
+        // mensaje de error
+        mensaje.textContent = "Error! ingrese un nombre en el input";
+    } else {
+        // grabo string en array
+        asistentes.push(asistente.value);
+        mensaje.textContent = "...respuesta";
+    }
+    asistente.value = "";
+});
+
+document.querySelector("#ver").addEventListener("click", () => {
+    //console.log("Boton ver clickeado");
+    const mensaje = document.querySelector("#listaAsistencia #mensaje");
+    mensaje.textContent = "";
+    if (asistentes.length === 0) {
+        // indicar que no hay datos
+        mensaje.textContent = "Error! No hay nombres guardados";
+    } else {
+        // mostrar los nombres del array
+        asistentes.forEach((nombre, i) => {
+            i++;
+            mensaje.innerHTML += `<div>${i} - ${nombre}</div>`;
+        });
+    }
+});
