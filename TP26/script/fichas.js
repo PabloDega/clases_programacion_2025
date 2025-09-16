5a22a1a42b5fed8f0f68e43f013c9d7c3ff723aa
Pablo Dega <pepeito@pepito.com>
2025-09-16 16:06:48 -0300

    TP26

--- TP26/script/fichas.js
+++ TP26/script/fichas.js
@@ -1,38 +1,62 @@
+// 1 Capturar los datos del usuario
+// 2 validar los datos
+    // 2-b mostrar error -> return
+// 3 Crear objeto con los datos y agregar objeto al array
+// 4 vaciar campos
+// 5 (opcional) mostrar largo del array al usuario
+
 document.querySelector("#registrar").addEventListener("click", () => {
-    console.log("Click a registrar");
-    // 1 Capturar los datos del usuario
     const nombre = document.querySelector("#nombre");
     const apellido = document.querySelector("#apellido");
     const telefono = document.querySelector("#telefono");
-    // 2 validar los datos
+    //console.log(nombre.value, apellido.value, telefono.value);
+
     if(nombre.value === "" || apellido.value === "" || telefono.value === ""){
-        // 2-b mostrar error
+        //console.log("Completar todos los input");
+        mostrarError("Debe completar los 3 campos de input para poder registrar la ficha");
         return;
     }
-    // 3 Crear instancia de clase y agregar al array
-    fichas.push(new Ficha(nombre.value, apellido.value, telefono.value))
-    // 4 vaciar campos
+
+    let datos = {
+        nombre: nombre.value,
+        apellido: apellido.value,
+        telefono: telefono.value
+    }
+
+    //console.log(datos);
+    fichas.push(datos);
+    console.log(fichas);
+
     nombre.value = "";
     apellido.value = "";
     telefono.value = "";
-    // 5 (opcional) mostrar largo del array al usuario
+
     document.querySelector("#vista h2").innerHTML = `Registros <span>${fichas.length}</span>`;
-});
-
-document.querySelector("#mostrar").addEventListener("click", () => {
-    console.log("Click a mostrar")
-});
-
-document.querySelector("#vaciar").addEventListener("click", () => {
-    console.log("Click a vaciar")
+    console.log("Finaliza la función");
 });
 
 let fichas = [];
 
-class Ficha{
-    constructor(nom, ape, tel){
-        this.nombre = nom;
-        this.apellido = ape;
-        this.telfono = tel;
-    }
+function mostrarError(txt){
+    const divErrores = document.querySelector("#errores");
+    divErrores.textContent = txt;
+    divErrores.style.display = "flex";
+    setTimeout(() => {
+        divErrores.style.display = "none";
+    }, 1000 * 10);
 }
+
+document.querySelector("#mostrar").addEventListener("click", () => {
+    //console.log("Click a btn mostrar");
+    if(fichas.length === 0){
+        mostrarError("No hay registros para mostrar");
+        return;
+    }
+    fichas.forEach((objeto) => {
+        document.querySelector("#salida").innerHTML += `<div class="ficha">
+                <span>Nombre: ${objeto.nombre}</span>
+                <span>Apellido: ${objeto.apellido}</span>
+                <span>Telefono: ${objeto.telefono}</span>
+            </div>`;
+    });
+})
