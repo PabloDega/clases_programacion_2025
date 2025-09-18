194983a5805adfc07335747890938be30a2ef4ce
Pablo Dega <pepeito@pepito.com>
2025-09-18 15:00:24 -0300

    TP27

---
+++ TP27/script/script.js
@@ -0,0 +1,61 @@
+let alumnos = [
+  {
+    id: 1,
+    nombre: "Ricardo",
+    apellido: "Gonzalez",
+    dni: 22544875,
+    localidad: "Tres de Febrero"
+  },
+  {
+    id: 2,
+    nombre: "Carla",
+    apellido: "Perez",
+    dni: 33522166,
+    localidad: "San Martín"
+  },
+  {
+    id: 3,
+    nombre: "José",
+    apellido: "Martinez",
+    dni: 41578688,
+    localidad: "Tres de Febrero"
+  },
+  {
+    id: 4,
+    nombre: "Paula",
+    apellido: "Gomez",
+    dni: 38168726,
+    localidad: "San Martín"
+  }
+];
+
+let cursos = [
+  {
+    id: 1,
+    nombre: "Programación Web",
+    calificado: false,
+    notas: [],
+    alumnos: [1, 2, 3, 4]
+  },
+  {
+    id: 2,
+    nombre: "Pensamiento computacional",
+    calificado: true,
+    notas: [10, 9],
+    alumnos: [2, 4]
+  },
+  {
+    id: 3,
+    nombre: "Sublimación",
+    calificado: false,
+    notas: [],
+    alumnos: [1, 2]
+  },
+  {
+    id: 4,
+    nombre: "Impresión 3D",
+    calificado: true,
+    notas: [10, 9, 5],
+    alumnos: [2, 3, 4]
+  }
+];
