953d9100410896a914f0d91e4fa92afaf3cd26ba
Pablo Dega <pepeito@pepito.com>
2025-11-25 13:20:26 -0300

    TP36

---
+++ TP36/index.js
@@ -0,0 +1,18 @@
+import express from "express";
+import {rutas} from "./src/routes/site.routes.js"
+const PORT = 4000;
+
+const app = express();
+
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: false }));
+
+app.use("/", rutas);
+
+app.use((req, res, next) => {
+  res.status(404).send('<h1>no se pudo encontrar esa página!</h1>');
+});
+
+app.listen(PORT, () => {
+    console.log(`Servidor activo en http://localhost:${PORT}`)
+})
