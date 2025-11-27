c2423f967e2bee692b4f07c1718996c449d7a836
Pablo Dega <pepeito@pepito.com>
2025-11-27 16:11:08 -0300

    TP36

--- TP36/src/controllers/formularios.controllers.js
+++ TP36/src/controllers/formularios.controllers.js
@@ -3,7 +3,7 @@ import {insertDatosFormulario} from "../services/formularios.services.js"
 export const formularioContacto = async (req, res) => {
     console.log("--> formularioContacto");
     const resp = await insertDatosFormulario(req.body);
-    // respuesta esperada del servicio {error: false} o {error:true, msg: "txt"}
+    // respuesta (resp) esperada del servicio insertDatosFormulario: {error: false} o {error:true, msg: "txt", err_code: 20-501}
     if(resp.error){
         res.send(`<h2>Error al grabar los datos: ${resp.msg}</h2>`);
         return;
--- TP36/src/services/formularios.services.js
+++ TP36/src/services/formularios.services.js
@@ -1,6 +1,13 @@
+import {query} from "../config/database.js"
+
 export const insertDatosFormulario = async (datos) => {
     console.log("--> insertDatosFormulario");
-    // proceso proceso proceso
-    return {error: true, msg: "Error 503"}
-    return {error: false}
+    try {
+        // intentar grabar en BD
+        const resp = await query(`INSERT into formulario_contacto (nombre, telefono) VALUES ("${datos.nombre}", ${datos.telefono})`);
+        return {error: false};
+    } catch (err) {
+        console.error("Error 20-501: " + err);
+        return {error: true, msg: err.msg};
+    }
 }
